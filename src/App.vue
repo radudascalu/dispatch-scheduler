a14b393b319bcc1cb6532567a5e0c423a2589c52
radudascalu <raduciprian.dascalu@gmail.com>
2021-02-12 12:17:18 +0200

    feat: add dispatch scheduler component skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dispatch-scheduler />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DispatchScheduler from './components/DispatchScheduler.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DispatchScheduler
   }
 }
 </script>
---
+++ src/components/DispatchScheduler.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    Dispatch Scheduler
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DispatchScheduler',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
